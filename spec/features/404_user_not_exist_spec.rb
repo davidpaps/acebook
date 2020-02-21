d961fb8c65472923e7e0019dd8d898a302ce3be1
Umberto Valente <uvalente89@gmail.com>
2020-02-21 14:17:01 +0000

    fix: small bug fix

--- spec/features/404_user_not_exist_spec.rb
+++ spec/features/404_user_not_exist_spec.rb
@@ -3,7 +3,7 @@
 feature 'user 404' do
   scenario 'a user access a url that does not correspond to a existing user' do
     sign_up
-    visit('/users/10')
+    visit('/users/randomusername')
     expect(page).to have_content("The user you were looking for doesn't exist")
   end
 end
